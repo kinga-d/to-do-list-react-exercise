a12e06ec21ba8ad83e21659fdb1b2873a71f71fc
Kinga Danielewicz <danielewicz.kinga@gmail.com>
2022-07-29 00:32:11 +0200

    set Todo

--- to-do-list-react-exercise/src/App.js
+++ to-do-list-react-exercise/src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
 
 function App() {
-    const [todos, setTodos] = useState([{id: 1, name:'Todo 1', complete: false}])
+    const [todos, setTodos] = useState([])
+    const todoNameRef = useRef()
+
+    function handleAddTodo(e) {
+        const name = todoNameRef.current.value
+        if (name === '') return
+   
+        todoNameRef.current.value = null
+    }
+
     return (
         <>
             <TodoList todos={todos} />
-            <input type="text" />
-            <button>Add Todo </button>
+            <input ref={todoNameRef} type="text" />
+            <button onClick={handleAddTodo}>Add Todo </button>
             <button> Clear Completed Todos </button>
             <div>0 left to do</div>
         </>
